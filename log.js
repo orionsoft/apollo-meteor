995e628b66d20be69a87c0e785b4cf37b93b3938
Nicolás López <nicolaslopezj@me.com>
2017-04-25 18:10:18 -0300

    fix logs

--- log.js
+++ log.js
@@ -1,17 +1,18 @@
 import {_} from 'meteor/underscore'
 
 export default function ({req, context}) {
-  console.info(`New GraphQL query ${req.body.operationName}:`.underline.bold)
+  const options = req.body[0]
+  console.info(`New GraphQL query ${options.operationName}:`.underline.bold)
   console.info(`Token: ${context.loginToken}\n`.grey)
-  console.info(req.body.query)
-  if (req.body.variables) {
+  console.info(options.query)
+  if (options.variables) {
     try {
-      if (_.isObject(req.body.variables)) {
+      if (_.isObject(options.variables)) {
         console.info('\n')
         console.info('Variables:'.underline)
-        console.info(req.body.variables)
+        console.info(options.variables)
       } else {
-        const obj = JSON.parse(req.body.variables)
+        const obj = JSON.parse(options.variables)
         if (!_.isEmpty(obj)) {
           console.info('\n')
           console.info('Variables:'.underline)
--- package.js
+++ package.js
@@ -2,7 +2,7 @@
 
 Package.describe({
   name: 'orionsoft:apollo',
-  version: '0.1.0',
+  version: '0.1.1',
   summary: 'A better Apollo integration for Meteor only serverside',
   git: 'https://github.com/orionsoft/apollo-meteor',
   documentation: 'README.md'
